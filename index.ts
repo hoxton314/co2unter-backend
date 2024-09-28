4fe5c0f41e28d2e20c74a3c72fd1c9f6608fc9cc
bjaworski2002 <bjaworski2002@gmail.com>
2024-09-28 12:50:09 +0200

    init-bug

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log('Thank you for using WebStorm 💙')
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req: Request, _res: Response) => {
+    _res.send('Express + TypeScript Server');
+});
+
+app.use((_req: Request, res: Response) => {
+    res.status(404).send("Not found");
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
